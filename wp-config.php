d69acb61d08022e7a432b98856df0b9b02ca58c7
muriukikelvin <43674768+muriukikelvin@users.noreply.github.com>
2020-09-12 03:30:08 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'monjeka' );
+define( 'DB_NAME', 'monjek' );
 
 /** MySQL database username */
 define( 'DB_USER', 'b3d5e7cc4460e4' );
